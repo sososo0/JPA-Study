5e1eadb3c09181ff212511a3a6dbd52a93de63e1
sososo0 <sh0000@pusan.ac.kr>
2024-02-17 17:05:07 +0900

    chore: project 세팅

---
+++ src/test/java/com/java/jpastudy/JpaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.jpastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
